b32bbc2507c9aa4c0efee3a6287828ee8b976336
lupingarrido0 <84425994+lupingarrido0@users.noreply.github.com>
2021-09-28 20:36:13 +0800

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -2,7 +2,10 @@ public class Menu {
     public static void main(String[] args) {
         MenuItem<MenuItem<MenuItem<String>>> mainMenu = new MenuItem<>("Google Slides");
         MenuItem<MenuItem<String>> fileRibbon = new MenuItem<>("File Operations");
-
+        
+        MenuItem<MenuItem<MenuItem<String>>> workingSpaceRibbon = new MenuItem<> ("Actual Working Space");
+        mainMenu.insert(workingSpaceRibbon);
+        workingSpaceRibbon.inputText();
 
         /*  TO TEST PROGRAM:
             mainMenu.insert() to add every ribbon to the main menu
